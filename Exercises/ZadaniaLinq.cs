580b88929bc7710dd220d7dee51844a9dd04ef58
Michal <s26133@pjwstk.edu.pl>
2026-04-30 22:24:47 +0200

    implementacja Zadanie14_SredniaOcenaNaPrzedmiot()

--- Exercises/ZadaniaLinq.cs
+++ Exercises/ZadaniaLinq.cs
@@ -258,7 +258,12 @@ public sealed class ZadaniaLinq
     /// </summary>
     public IEnumerable<string> Zadanie14_SredniaOcenaNaPrzedmiot()
     {
-        throw Niezaimplementowano(nameof(Zadanie14_SredniaOcenaNaPrzedmiot));
+        var result = DaneUczelni.Zapisy
+            .Where(z => z.OcenaKoncowa != null)
+            .Join(DaneUczelni.Przedmioty, z => z.PrzedmiotId, p => p.Id, (z, p) => new { p.Nazwa, z.OcenaKoncowa })
+            .GroupBy(x => x.Nazwa)
+            .Select(g => $"{g.Key} - {g.Average(x => x.OcenaKoncowa)}");
+        return result;
     }
 
     /// <summary>
